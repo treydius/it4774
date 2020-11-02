2a4049d194b68ceb2560f92ec78f37867fd59b32
treydius <68821944+treydius@users.noreply.github.com>
2020-11-01 19:55:51 -0600

    Add files via upload

--- ProgrammableThermostat/src/programmablethermostat/ProgrammableThermostat.java
+++ ProgrammableThermostat/src/programmablethermostat/ProgrammableThermostat.java
@@ -1,18 +1,21 @@
 package programmablethermostat;
 /** @author CharlesAllen */
 
-//import Scanner utility to be able to read user input
+//import for option 1
 import java.io.*;
 import java.net.URL;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
+//imports for option 2
+import java.net.HttpURLConnection;
 
 public class ProgrammableThermostat {
 
     public static void main(String[] args) throws Exception {
         
-        //create a URL object to reference the JSON web file
+        ///////////   OPTION 1: CODE BELOW IS USING inputstreamreader and openStream to create a connection //////////////
+        //create a URL object to reference the JSON web file        
         URL url = new URL("http://media.capella.edu/BBCourse_Production/IT4774/temperature.json");
         
         //create a BufferedReader object to read the file using input stream
@@ -33,6 +36,34 @@ public class ProgrammableThermostat {
         //output to console
         System.out.println("Actual Temperature is " + actualTemp);
         
+        
+        
+        
+        ///////////   OPTION 2: CODE BELOW IS USING HttpURLConnection to create a connection before passing to InputStreamReader  //////////////
+        /*
+        URL url = new URL("http://media.capella.edu/BBCourse_Production/IT4774/temperature.json");
+        
+        //creates a URL connection using the URL object
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      
+        //create a BufferedReader object to read the file using InputStreamReader getInputStream off the connection
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        
+        //create parser to parse through data
+        JSONParser parser = new JSONParser();
+        
+        //create java objects using the parser
+        JSONObject json = (JSONObject) parser.parse(reader);
+        
+        //variable to store the runtime object
+        JSONObject runtime = (JSONObject) json.get("runtime");
+        
+        //variable to store the actual temperature
+        int actualTemp = Integer.parseInt(runtime.get("actualTemperature").toString());
+        
+        //output to console
+        System.out.println("Actual Temperature is " + actualTemp);
+        */
 
 
         
